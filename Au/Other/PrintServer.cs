e5acb18140adf54389a829d359f6f1ac5d4a6801
dksyks <gihub.com@dks.selig.us>
2025-03-02 21:13:27 -0500

    Changes to PrintServer Documentation

--- Au/Other/PrintServer.cs
+++ Au/Other/PrintServer.cs
@@ -31,88 +31,92 @@ How local output server/client is implemented:
 */
 
 namespace Au.More {
-	/// <summary>
-	/// Receives messages sent by <see cref="print.it"/>.
-	/// </summary>
-	/// <remarks>
-	/// If server is global, clients can be multiple processes, including this. Else only this process.
-	/// Works asynchronously, to make writing messages faster.
-	/// When a client writes a message, the message arrives to the server with some delay and is placed in a queue.
-	/// You then can get/remove messages from the queue (call <see cref="GetMessage"/>) and display them in a window (for example).
-	/// You can be notified about new messages.
-	/// 
-	/// Recommended setup (see example):
-	/// 1. When your application starts, create a <b>PrintServer</b> instance and assign to a static variable. Call <see cref="Start"/>.
-	/// 2. When your application creates its output window, call <see cref="SetNotifications"/> to set window/message for notifications.
-	/// 3. In window procedure, when received the notification message, get/remove/display all new output messages.
-	/// 4. Call <see cref="Stop"/> when closing the window.
-	/// </remarks>
-	/// <example>
-	/// Simple program with output window.
-	/// <code><![CDATA[
-	/// using System.Windows.Forms;
-	/// 
-	/// class OutputFormExample : Form {
-	/// 	TextBox _tb;
-	/// 
-	/// 	public OutputFormExample()
-	/// 	{
-	/// 		_tb = new TextBox();
-	/// 		_tb.ReadOnly = true;
-	/// 		_tb.Multiline = true;
-	/// 		_tb.ScrollBars = ScrollBars.Both;
-	/// 		_tb.WordWrap = false;
-	/// 		_tb.Dock = DockStyle.Fill;
-	/// 		_tb.TabStop = false;
-	/// 		this.Controls.Add(_tb);
-	/// 	}
-	/// 
-	/// 	protected override void OnHandleCreated(EventArgs e) {
-	/// 		_os.SetNotifications(this.Hwnd(), WM_APP);
-	/// 		base.OnHandleCreated(e);
-	/// 	}
-	/// 
-	/// 	protected override void WndProc(ref Message m) {
-	/// 		if(m.Msg == WM_APP) _ProcessMessages();
-	/// 		base.WndProc(ref m);
-	/// 	}
-	/// 	
-	/// 	internal const int WM_APP = 0x8000;
-	/// 
-	/// 	void _ProcessMessages()
-	/// 	{
-	/// 		while(_os.GetMessage(out var m)) {
-	/// 			switch(m.Type) {
-	/// 			case PrintServerMessageType.Clear:
-	/// 				_tb.Clear();
-	/// 				break;
-	/// 			case PrintServerMessageType.Write:
-	/// 				//_tb.AppendText(m.Text);
-	/// 				_tb.AppendText($"{DateTime.FromFileTimeUtc(m.TimeUtc).ToLocalTime()}  {m.Caller}  {m.Text}");
-	/// 				break;
-	/// 			}
-	/// 		}
-	/// 	}
-	/// 
-	/// 	static PrintServer _os = new(isGlobal: false);
-	/// 
-	/// 	[STAThread]
-	/// 	static void Main()
-	/// 	{
-	/// 		_os.Start();
-	/// 
-	/// 		//test Write and Clear, before and after creating window
-	/// 		print.ignoreConsole = true;
-	/// 		print.it("test before setting notifications");
-	/// 		Task.Run(() => { 1.s(); print.it("test after"); 1.s(); print.clear(); 1.s(); print.it("test after Clear"); });
-	/// 
-	/// 		Application.Run(new OutputFormExample());
-	/// 		_os.Stop();
-	/// 	}
-	/// }
-	/// ]]></code>
-	/// </example>
-	public unsafe class PrintServer {
+    /// <summary>
+    /// Receives messages sent by <see cref="print.it"/>.
+    /// </summary>
+    /// <remarks>
+    /// If server is global (see constructor), clients can be multiple processes, including this, but there is only a single active global server.  If local then it is only the print server for this process.
+    /// Works asynchronously, to make writing messages faster.
+    /// When a client writes a message, the message arrives to the server with some delay and is placed in a queue.
+    /// You then can get/remove messages from the queue (call <see cref="GetMessage"/>) and display them in a window (for example).
+    /// You can be notified about new messages.
+    /// 
+    /// Recommended setup (see example):
+    /// 1. When your application starts, create a <b>PrintServer</b> instance and assign to a static variable. Call <see cref="Start"/>.
+    /// 2. When your application creates its output window, call <see cref="SetNotifications"/> to set window/message for notifications.
+    /// 3. In window procedure, when received the notification message, get/remove/display all new output messages.
+    /// 4. Call <see cref="Stop"/> when closing the window.
+    /// </remarks>
+    /// <example>
+    /// Simple program with output window.
+    /// <code><![CDATA[
+    /// /*/ role exeProgram; outputPath %folders.Workspace%\exe\OutputFormExample; /*/
+    /// using System.Windows.Forms;
+    /// 
+    /// class OutputFormExample : Form {
+    /// 	TextBox _tb;
+    /// 
+    /// 	public OutputFormExample()
+    /// 	{
+    /// 		_tb = new TextBox();
+    /// 		_tb.ReadOnly = true;
+    /// 		_tb.Multiline = true;
+    /// 		_tb.ScrollBars = ScrollBars.Both;
+    /// 		_tb.WordWrap = false;
+    /// 		_tb.Dock = DockStyle.Fill;
+    /// 		_tb.TabStop = false;
+    /// 		this.Controls.Add(_tb);
+	/// 		this.width = 1000;
+	/// 		this.height = 500;
+    /// 	}
+    /// 
+    /// 	protected override void OnHandleCreated(EventArgs e) {
+    /// 		_printServer.SetNotifications(this.Hwnd(), WM_APP);
+    /// 		base.OnHandleCreated(e);
+    /// 	}
+    /// 
+    /// 	protected override void WndProc(ref Message m) {
+    /// 		if(m.Msg == WM_APP) _ProcessMessages();
+    /// 		base.WndProc(ref m);
+    /// 	}
+    /// 	
+    /// 	internal const int WM_APP = 0x8000;
+    /// 
+    /// 	void _ProcessMessages()
+    /// 	{
+    /// 		while(_printServer.GetMessage(out var m)) {
+    /// 			switch(m.Type) {
+    /// 			case PrintServerMessageType.Clear:
+    /// 				_tb.Clear();
+    /// 				break;
+    /// 			case PrintServerMessageType.Write:
+    /// 				//_tb.AppendText(m.Text);
+    /// 				_tb.AppendText($"{DateTime.FromFileTimeUtc(m.TimeUtc).ToLocalTime()}  {m.Caller}  {m.Text}");
+    /// 				break;
+    /// 			}
+    /// 		}
+    /// 	}
+    /// 
+	///		// if try isGlobal: true, can get output here but need to close LA, otherwise output goes there
+    /// 	static PrintServer _printServer = new(isGlobal: false);
+    /// 
+    /// 	[STAThread]
+    /// 	static void Main()
+    /// 	{
+    /// 		_printServer.Start();
+    /// 
+    /// 		//test Write and Clear, before and after creating window
+    /// 		print.ignoreConsole = true;
+    /// 		print.it("test before setting notifications");
+    /// 		Task.Run(() => { 2.s(); print.it("test after"); 2.s(); print.clear(); 2.s(); print.it("test after Clear"); });
+    /// 
+    /// 		Application.Run(new OutputFormExample());
+    /// 		_printServer.Stop();
+    /// 	}
+    /// }
+    /// ]]></code>
+    /// </example>
+    public unsafe class PrintServer {
 		//info:
 		//Although global and local servers are implemented quite differently, the interface is almost the same. For this and other reasons I decided to use single class.
 		//For local server, the thread and kernel timer would be not necessary. Instead could use just a user timer. But it has some limitations etc.
@@ -127,7 +131,7 @@ namespace Au.More {
 		bool _isLocalTimer;
 		
 		/// <param name="isGlobal">
-		/// If <c>true</c>, will receive output from all processes that don't have local server.
+		/// If <c>true</c>, will receive output from all processes that don't have local server.  Only one global server receives messages.  By default this is the output panel of the code editor if it is running.
 		/// </param>
 		public PrintServer(bool isGlobal) => _isGlobal = isGlobal;
 		
